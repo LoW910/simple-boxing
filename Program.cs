4c8fed32867077f1559c8885251c4b0bdeb22e4f
Daniel Soots <danielsoots@icloud.com>
2022-01-21 09:10:32 -0500

    created list obj, added values, unboxed from obj, implicit cast as int if obj is int then printed sum

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Boxing
 {
@@ -7,6 +8,56 @@ namespace Boxing
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            object BoxedData = "This is clearly a string";
+
+            if(BoxedData is int) {
+                System.Console.WriteLine("I guess we have an integer value in this box");
+            }
+            if (BoxedData is string) {
+                System.Console.WriteLine("It is totally a string in the box!");
+            }
+
+
+            object actuallyString = "a string";
+            string explicitString = actuallyString as string;
+
+            // // THIS WON'T WORK!!
+            // object actuallyInt = 256;
+            // int explicitInt = actuallyInt as int;
+
+            List<object> newObjectList = new List<object>();
+            newObjectList.Add(7);
+            newObjectList.Add(28);
+            newObjectList.Add(-1);
+            newObjectList.Add(true);
+            newObjectList.Add("chair");
+
+            System.Console.WriteLine(newObjectList);
+
+            int num = 0;
+            foreach (var obj in newObjectList)
+            {
+                System.Console.WriteLine(obj);
+                if(obj is int) {
+                    num += (int) obj;
+                }
+            }
+            System.Console.WriteLine(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
